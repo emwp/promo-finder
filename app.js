48a635e60ce6eeb4baba2a648db8afb8bd455a1e
Everton Pereira <everton.mwp@gmail.com>
2019-02-26 01:58:01 -0300

    connecting to the mongodb cluster with mongoose

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const graphqlHttp = require('express-graphql');
 const { buildSchema } = require('graphql');
+const mongoose = require('mongoose');
 
 const app = express();
 
@@ -62,4 +63,13 @@ app.use(
   }),
 );
 
-app.listen(3000);
+mongoose
+  .connect(
+    `mongodb+srv://${process.env.MONGO_USER}:${
+      process.env.MONGO_PASSWORD
+    }@cluster0-j9chw.mongodb.net/test?retryWrites=true`,
+  )
+  .then(() => {
+    app.listen(3000);
+  })
+  .catch(err => console.log(err));
