694799be7440a3cedb03f5ed7f53fa2d6650746e
Everton Pereira <everton.mwp@gmail.com>
2019-02-27 20:40:11 -0300

    added login resolver

--- graphql/resolvers/auth.js
+++ graphql/resolvers/auth.js
@@ -1,5 +1,6 @@
 const bcrypt = require('bcryptjs');
 const User = require('../../models/user');
+const jwt = require('jsonwebtoken');
 
 module.exports = {
   // Graphql mutation to create new users
@@ -21,5 +22,19 @@ module.exports = {
     } catch (err) {
       throw err;
     }
+  },
+  login: async ({ email, password }) => {
+    const user = User.findOne({ email: email });
+    if (!user) {
+      throw new Error('User does not exist!');
+    }
+    const passMatch = await bcrypt.compare(password, user.password);
+    if (!passMatch) {
+      throw new Error('Password is incorrect');
+    }
+    const token = await jwt.sign({ userId: user.id, email: user.email }, 'secretkeyfortesting', {
+      expiresIn: '1h',
+    });
+    return { userId: user.id, token: token, tokenExpiration: 1 };
   },
 };
