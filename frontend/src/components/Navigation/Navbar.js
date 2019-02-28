26562aaf8547fb44ace24de447643158ed9c4775
Everton Pereira <everton.mwp@gmail.com>
2019-02-28 17:43:32 -0300

    added navbar component

--- frontend/src/components/Navigation/Navbar.js
+++ frontend/src/components/Navigation/Navbar.js
@@ -1,11 +1,15 @@
-import React, { useState } from 'react';
+import React from 'react';
 import styled from 'styled-components';
+import NavbarLinks from './NavbarLinks';
+// import NavbarIcons from './NavbarIcons';
 
 const Navbar = () => {
   return (
     <NavWrapper>
+      <div className="nav_logo">
+        <h1>PromoFinder</h1>
+      </div>
       <NavbarLinks />
-      <NavbarIcons />
     </NavWrapper>
   );
 };
@@ -15,9 +19,18 @@ export default Navbar;
 // CSS
 
 const NavWrapper = styled.nav`
-  /* background: #28a19f; */
-  @media (min-width: 768px) {
-    display: flex;
-    align-items: center;
+  position: fixed;
+  left: 0;
+  top: 0;
+  width: 100%;
+  display: flex;
+  height: 3.5rem;
+  background: red;
+  align-items: center;
+  justify-content: space-around;
+
+  .nav_logo h1 {
+    margin: 0;
+    font-size: 1.5rem;
   }
 `;
