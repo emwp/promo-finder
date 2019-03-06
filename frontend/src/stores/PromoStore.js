6bc53a01003b4e9f2293acddc2183f30822dccfa
Everton Pereira <everton.mwp@gmail.com>
2019-03-06 15:51:04 -0300

    added PromoStore observable data

--- frontend/src/stores/PromoStore.js
+++ frontend/src/stores/PromoStore.js
@@ -3,10 +3,18 @@ import { createContext } from 'react';
 
 class PromoStore {
   creatingPromo = false;
+  title = '';
+  description = '';
+  price = '';
+  date = '';
 }
 
 decorate(PromoStore, {
   creatingPromo: observable,
+  title: observable,
+  description: observable,
+  price: observable,
+  date: observable,
 });
 
 export const PromoStoreContext = createContext(new PromoStore());
