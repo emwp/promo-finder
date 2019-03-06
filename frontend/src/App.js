103f84255737aa4a6d70911f1e998544c042026d
Everton Pereira <everton.mwp@gmail.com>
2019-03-06 18:18:21 -0300

    Refactored App.js to functional component && Redirect when signin

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,28 +1,30 @@
-import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import React, { useContext } from 'react';
+import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
 import Promos from './containers/Promos';
 import Navbar from './components/Navigation/Navbar';
 import './App.css';
 import Login from '../src/containers/Auth/Login';
 import Register from '../src/containers/Auth/Register';
+import { observer } from 'mobx-react-lite';
+import { AuthStoreContext } from './stores/AuthStore';
 
-class App extends Component {
-  render() {
-    return (
-      <Router>
-        <React.Fragment>
-          <Navbar />
-          <main className="main-content">
-            <Switch>
-              <Route exact path="/" component={Promos} />
-              <Route exact path="/login" component={Login} />
-              <Route exact path="/register" component={Register} />
-            </Switch>
-          </main>
-        </React.Fragment>
-      </Router>
-    );
-  }
-}
+const App = observer(() => {
+  const authStore = useContext(AuthStoreContext);
+  return (
+    <Router>
+      <React.Fragment>
+        <Navbar />
+        <main className="main-content">
+          <Switch>
+            {authStore.token && <Redirect from="/login" to="/" exact />}
+            <Route exact path="/" component={Promos} />
+            <Route exact path="/login" component={Login} />
+            <Route exact path="/register" component={Register} />
+          </Switch>
+        </main>
+      </React.Fragment>
+    </Router>
+  );
+});
 
 export default App;
