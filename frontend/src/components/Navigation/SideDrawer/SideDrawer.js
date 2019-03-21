46e87e52e913733a995abadd06025e1d790a5886
Everton Pereira <everton.mwp@gmail.com>
2019-03-21 15:07:20 -0300

    add contact icons to SideDrawer.js

--- frontend/src/components/Navigation/SideDrawer/SideDrawer.js
+++ frontend/src/components/Navigation/SideDrawer/SideDrawer.js
@@ -3,6 +3,8 @@ import { NavLink } from 'react-router-dom';
 import styled from 'styled-components';
 import { observer } from 'mobx-react-lite';
 import { AuthStoreContext } from '../../../stores/AuthStore';
+import GitHub from '../../../img/github.svg';
+import LinkedIn from '../../../img/in.svg';
 
 const SideDrawer = observer(props => {
   const authStore = useContext(AuthStoreContext);
@@ -33,6 +35,18 @@ const SideDrawer = observer(props => {
           </NavLink>
         ) : null}
       </ul>
+      <div className="social_links">
+        <a href="https://github.com/emwp" target="_blank" rel="noopener noreferrer">
+          <img src={GitHub} alt="GitHub" />
+        </a>
+        <a
+          href="https://www.linkedin.com/in/everton-pereira-1588a1105/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img src={LinkedIn} alt="LinkedIn" />
+        </a>
+      </div>
     </WrapperSideDrawer>
   );
 });
@@ -62,5 +76,21 @@ const WrapperSideDrawer = styled.div`
     margin-bottom: 1rem;
     text-decoration: none;
     color: #fe6756;
+  }
+
+  .social_links {
+    margin-top: 10%;
+    margin-left: 10%;
+    display: flex;
+  }
+
+  .social_links a {
+    cursor: auto;
+    margin: 0 1rem;
+  }
+
+  .social_links img {
+    width: 1.3rem;
+    cursor: pointer;
   }
 `;
