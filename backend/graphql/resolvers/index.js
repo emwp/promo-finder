3438b5e81ea398a524778987943891de08b566ef
Everton Pereira <everton.mwp@gmail.com>
2019-03-25 20:55:06 -0300

    update folder structure

--- backend/graphql/resolvers/index.js
+++
@@ -1,9 +0,0 @@
-const authResolver = require('./auth');
-const promosResolver = require('./promos');
-
-const rootResolver = {
-  ...authResolver,
-  ...promosResolver,
-};
-
-module.exports = rootResolver;
